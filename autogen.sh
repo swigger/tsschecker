f8644fabb1adb04fc0ee42860fd7d2e0469823dc
Luke Street <luke.street@encounterpc.com>
2017-12-27 00:57:18 -0500

    Add libfragmentzip submodule

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 gprefix=`which glibtoolize 2>&1 >/dev/null`
 if [ $? -eq 0 ]; then
   glibtoolize --force
@@ -7,11 +6,22 @@ else
   libtoolize --force
 fi
 aclocal -I m4
-autoconf
 autoheader
 automake --add-missing
+autoconf
 
-if [ -z $NOCONFIGURE ]; then
-  ./configure "$@"
+export NOCONFIGURE=1
+
+SUBDIRS="external/libfragmentzip"
+for SUB in $SUBDIRS; do
+    pushd $SUB
+    ./autogen.sh
+    popd
+done
+
+unset NOCONFIGURE
+
+if [ -z "$NOCONFIGURE" ]; then
+    ./configure "$@"
 fi
 ./setBuildVersion.sh
